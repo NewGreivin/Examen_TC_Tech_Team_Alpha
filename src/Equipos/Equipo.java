da7b056184deef76f2f997c6bfc85653382e88ff
Marisol <marialfarolo@est.utn.ac.cr>
2025-07-03 14:35:24 -0600

    Se crea la clase Equipo
    Contiene los atributos solicitados, los gets, el constructor y el toString

---
+++ src/Equipos/Equipo.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Equipos;
+
+/**
+ *
+ * @author Student
+ */
+public class Equipo {
+    private String codigo;
+    
+    private String descripcion;
+    
+    private String ubicacion;
+    
+    private EnumEstado estado;
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public EnumEstado getEstado() {
+        return estado;
+    }
+
+    public Equipo(String codigo, String descripcion, String ubicacion, EnumEstado estado) {
+        this.codigo = codigo;
+        this.descripcion = descripcion;
+        this.ubicacion = ubicacion;
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipo:" + "codigo=" + codigo + ", descripcion=" + descripcion + ", ubicacion=" + ubicacion + ", estado=" + estado;
+    }
+    
+    
+}
